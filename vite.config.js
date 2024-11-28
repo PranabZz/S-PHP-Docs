1acc420f7f29d8f595bf8a4a10f3e2c52697bbe5
PranabZz <pranabkca321@gmail.com>
2024-11-28 15:57:21 +0545

    vercel config

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import { vitePress } from 'vitepress'
-
-export default defineConfig({
-  plugins: [vitePress()],
-  build: {
-    outDir: 'dist', // This is important for Vercel
-  }
-})
