3cabb83a84a2d680ca14d97506930b06a666bbff
PranabZz <pranabkca321@gmail.com>
2024-11-28 20:30:54 +0545

    Config modified

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '/', // Change this to the subpath if your app is not at the root
   build: {
-    outDir: '.vitepress/dist', // Ensure this matches your build command output directory
+    outDir: '/dist', // Ensure this matches your build command output directory
     emptyOutDir: true,
   },
   server: {
